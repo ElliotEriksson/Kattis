8bd173765a6350d94f698e2051eeee0c248c8fa2
ElliotEriksson <elliot@serait.se>
2022-09-09 12:40:03 +0200

    Variable Change

--- Kattis-Lamps/kattis-lamps.py
+++ Kattis-Lamps/kattis-lamps.py
@@ -1,9 +1,9 @@
 H, P = list(map(int, input().split()))
-D = 0
+Days = 0
 
-C1, C2 = 0, 0
-while C1 <= C2:
-    D += 1
-    C1 = (D * H + 999) // 1000 * 5 + (60 * D * H * P) / 10**5
-    C2 = (D * H + 7999) // 8000 * 60 + (11 * D * H * P) / 10**5
-print(D)
+Cost_bulb, Cost_led = 0, 0
+while Cost_bulb <= Cost_led:
+    Days += 1
+    Cost_bulb = (Days * H + 999) // 1000 * 5 + (60 * Days * H * P) / 10**5
+    Cost_led = (Days * H + 7999) // 8000 * 60 + (11 * Days * H * P) / 10**5
+print(Days)
